020b4ef05f36c3130cfe079fc94ed8a088ef4b53
Giridhara Chintada <gchintada@reputation.com>
2021-11-27 00:32:10 +0530

    Created the "BooStrapData" to initialize the spring data repository and inject data
    Created Files(1):
    1. BootStrapData.java: To initialize the existing repository objects and operate on Author and Book data
    
    Updated Files(3):
    1. Book.java: Initializing the authors during declaration
    2. Author.java: Initializing the books during declaration
    3. .gitignore: Added the file name ".java-version" to skip from future commits
    
    Deleted Files(0): None

---
+++ src/main/java/com/example/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,43 @@
+package com.example.spring5webapp.bootstrap;
+
+import com.example.spring5webapp.domain.Author;
+import com.example.spring5webapp.domain.Book;
+import com.example.spring5webapp.repositories.AuthorRepository;
+import com.example.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", 12345678L);
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", 98765432L);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started bootstrap!!!");
+        System.out.println("#Books: " + bookRepository.count());
+        System.out.println("#Authors: " + authorRepository.count());
+    }
+}
