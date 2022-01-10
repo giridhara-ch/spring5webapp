646a5bbc2a547e86247cdfed15f089817b898b1d
Giridhara Chintada <gchintada@reputation.com>
2022-01-10 22:43:56 +0530

    Added Thymeleaf template for Authors, AuthorController
    Created Files(2):
    1. AuthorController.java - Controller for handling "/author" url requests
    2. authors/list.html - To resolve the view for above controller getAuthors()
    
    Updated Files(1):
    1. Author.java - Added getter for id and firstName
    
    Deleted Files(0): None

--- src/main/java/com/example/spring5webapp/domain/Author.java
+++ src/main/java/com/example/spring5webapp/domain/Author.java
@@ -1,6 +1,10 @@
 package com.example.spring5webapp.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -10,19 +14,24 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String firstName;
     private String lastName;
-
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
-
     public Author() {
     }
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 
     @Override
